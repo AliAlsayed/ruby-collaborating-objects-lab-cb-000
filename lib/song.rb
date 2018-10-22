5dea1cf78e7996171eb3bc2d5189ad107e4756ff
Ali Alsayed <a_alsayed87@hotmail.com>
2018-10-22 08:04:04 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,6 +7,9 @@ class Song
   def self.new_by_filename(file)
     file = file.split("-")
     song = self.new(file[1].strip)
-    #song.artist = Artist.new(file[1].strip)
+    artist = Artist.new(file[1].strip)
+    song.artist =  artist
+    artist.add_song(song)
+    
   end
 end
