e641d51c81fb3edc63b935eeff4f267d1ebfb5a2
Ali Alsayed <a_alsayed87@hotmail.com>
2018-10-22 06:03:21 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,21 @@
+class Artist
+  attr_accessor :name, :songs
+  @@all = []
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    song. artist = self
+    @songs << song
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+end
